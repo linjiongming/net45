7e6576b06d02cf5841bf9035db14a4d1698fc7ee
linjiongming <linjiongming@163.com>
2025-03-20 16:00:47 +0800

    net45.Microsoft.Extensions.Logging

---
+++ net45.Microsoft.Extensions.Logging/DefaultLoggerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Microsoft.Extensions.Logging
+{
+    public static class DefaultLoggerExtensions
+    {
+        public static readonly EventId DefaultEventId = new EventId(0, "Default");
+
+        public static void LogError(this ILogger logger, Exception exception, string message, params object[] args)
+        {
+            logger.LogError(DefaultEventId, exception, message, args);
+        }
+    }
+}
