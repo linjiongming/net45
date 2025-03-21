6c51359f73d4aeee2a903427a7061f87cd6972b7
linjiongming <linjiongming@163.com>
2025-03-21 10:58:41 +0800

    data

---
+++ net45.System.Data/ServiceCollectionDatabaseExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionDatabaseExtensions
+    {
+        public static IServiceCollection AddDbRepositories(this IServiceCollection services, string name)
+        {
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[name];
+            DbProviderFactory factory = DbProviderFactories.GetFactory(settings.ProviderName);
+            services.AddTransient<IDbConnection>(provider =>
+            {
+                DbConnection connection = factory.CreateConnection();
+                connection.ConnectionString = settings.ConnectionString;
+                return connection;
+            });
+            Type iType = typeof(IDbRepository);
+            System.Collections.Generic.IEnumerable<Type> types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.ExportedTypes)
+                .Where(t => t.IsClass && !t.IsAbstract && iType.IsAssignableFrom(t));
+            foreach (Type type in types)
+            {
+                services.AddScoped(type, type);
+            }
+            return services;
+        }
+    }
+}
