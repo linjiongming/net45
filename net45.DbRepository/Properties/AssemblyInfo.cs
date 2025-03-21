3a2ed1a0b9fb7e72389a184fed7f439c706de092
linjiongming <linjiongming@163.com>
2025-03-21 11:58:24 +0800

    DbRepository

--- net45.DbRepository/Properties/AssemblyInfo.cs
+++ net45.DbRepository/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("net45.System.Data")]
+[assembly: AssemblyTitle("net45.DbRepository")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("net45.System.Data")]
+[assembly: AssemblyProduct("net45.DbRepository")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("1b9adf56-45e8-49f9-b302-c747c1d59e93")]
+[assembly: Guid("b86e2a50-24cc-408a-a5ed-c5d0d00a5a1a")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
