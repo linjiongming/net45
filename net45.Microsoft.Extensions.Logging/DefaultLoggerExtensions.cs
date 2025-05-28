618f422000017c91d39c343203c27103486a8fd4
linjiongming <linjiongming@163.com>
2025-05-28 10:41:20 +0800

    logging

--- net45.Microsoft.Extensions.Logging/DefaultLoggerExtensions.cs
+++ net45.Microsoft.Extensions.Logging/DefaultLoggerExtensions.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace Microsoft.Extensions.Logging
 {
     public static class DefaultLoggerExtensions
     {
-        public static readonly EventId DefaultEventId = new EventId(0, "Default");
+        private static readonly ConcurrentDictionary<string, EventId> _eventIdCache = new ConcurrentDictionary<string, EventId>();
+        private static readonly EventId _defaultEventId = new EventId(0, "Default");
 
         public static void LogError(this ILogger logger, Exception exception, string message, params object[] args)
         {
-            logger.LogError(DefaultEventId, exception, message, args);
+            var name = exception?.TargetSite?.DeclaringType?.FullName;
+            var eventId = string.IsNullOrWhiteSpace(name) ? _defaultEventId : _eventIdCache.GetOrAdd(name, n => new EventId(n.GetHashCode(), n));
+            logger.LogError(eventId, exception, message, args);
         }
     }
 }
